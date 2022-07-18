20c4e3ca229647dad42a383f59c58f2d8ac4d662
Rishi Tiku <52333443+RishiTiku@users.noreply.github.com>
2022-07-19 01:55:43 +0530

    Add files via upload

--- 9. Exception Handling/Prog3/tester.java
+++ 9. Exception Handling/Prog3/tester.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -57,9 +59,46 @@ class Cricketer{
         }
     }
     public int get_avg(){return batting_avg;}
+    public void display(){}
 }
 
+class TeamException extends Throwable{}
+
+class Sortbyavg implements Comparator<Cricketer> { 
+    public int compare(Cricketer a, Cricketer b) 
+    { 
+        return a.get_avg() - b.get_avg(); 
+    } 
+} 
+  
 
 public class tester {
-    
+    public static void main(String args[]) throws TeamException
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        do{
+            try{
+                System.out.println("Enter Number of Cricketers");
+                n = sc.nextInt();
+                break;
+            }
+            catch(InputMismatchException i)
+            {
+                System.out.println("Invalid Input Entered.");
+            }
+            sc.nextLine();
+        }while(true);
+        Cricketer A[] = new Cricketer[n];
+        for(int i = 0; i<n; i++)
+        {
+            A[i] = new Cricketer();
+        }
+        Arrays.sort(A, new Sortbyavg());
+        for(int i = 0; i<n; i++)
+        {
+            A[i].display();
+        }
+
+    }
 }
